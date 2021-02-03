29a4dbf52ddf3c22c434423e283dd76bef6a91d2
Angel Deluna <angelluna2016@gmail.com>
2021-02-03 15:49:33 -0500

    finished exercises

--- exercises/level3.js
+++ exercises/level3.js
@@ -1,24 +1,32 @@
 function lvl3exercise1 () {
   // Create a "hello" and a "world", return the concatenation of the two
-
+  let hello = "hello";
+  let world = "world";
+  return hello + world;
 }
 
 function lvl3exercise2 () {
   // Create a "hello" and a 12, return the concatenation of the two
-
+  let hello = "hello";
+  let num = 12;
+  return hello + num;
 }
 
 function lvl3exercise3 () {
   // Create a variable that equals 12 and convert it to a string with concatenation. Return it.
-
+  let num = 12;
+  return "" + 12;
 }
 
 function lvl3exercise4 () {
   // Create a "hello world!" string. Return the length of the string
+  let word = "hello world!";
+  return word.length;
 
 }
 
 function lvl3exercise5 () {
   // Create a "hello world!" string. Return the index of the word "world".
-
+  let text = "hello world!";
+  return text.indexOf("world");
 }
